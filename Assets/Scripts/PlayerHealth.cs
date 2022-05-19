4590ffd4ca8fca8cbb515bf7fc31b40fc3263c2e
Austin <metke70.am@gmail.com>
2022-05-19 16:36:53 -0700

    Implemented health bar for player, could be used to implement enemy healthbars as well

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
 
+    
     [SerializeField] private float playerHealth;
     private const float maxHealth = 100;
     private const float enemyDamage = 15;
     private bool isDead;
+    public HealthBar healthBar;
     // Start is called before the first frame update
     void Start()
     {
         playerHealth = maxHealth;
         isDead = false;
+        healthBar.setMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
@@ -22,18 +25,23 @@ public class PlayerHealth : MonoBehaviour
         if(playerHealth <= 0)
         {
             isDead = true;
+            Debug.Log("Player died!");
         }
-        
-
-
-
     }
     //Checks for collison with enemy
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
-            playerHealth -= enemyDamage;
+            Debug.Log("Player hit!");
+            takeDamage(enemyDamage);
         }
+    }
+
+    private void takeDamage(float damage)
+    {
+        playerHealth -= damage;
+        healthBar.setHealth(playerHealth);
+
     }
 }
